303696e52ee9bee0baa95469b2aa050aa54378f2
unknown <chr.strucks@gmail.com>
2018-03-07 20:44:31 +0100

    bug fixes in cropping and additional region labeling as basis for further cropping

--- cropping.py
+++ cropping.py
@@ -9,20 +9,20 @@ from sklearn import metrics
 import pandas as pd
 import random
 import matplotlib.image as mpimg
-
-
-
 import os
-#dir = os.path.dirname(__file__)
-#filename = dir + "/data/train/" + "e9162eee.jpg"
 
-#img_file = Image.open(filename + "fa62a2e8.jpg")
+dir = os.path.dirname(__file__)
+#filename = dir + "/data/train/"
+#
+#img_file = Image.open(filename + "e9162eee.jpg")
 #img = img_file.load()
 
-train_data = pd.read_csv('data/train.csv')
+train_data = pd.read_csv(dir + '/data/train.csv')
 
-filename = os.path.join('data/train', train_data.Image[random.randrange(0,9850)])
-#
+# filename = os.path.join(dir + '/data/train', train_data.Image[random.randrange(0, 9850)])
+filename = os.path.join(dir + '/data/train', "ddba0df3.jpg")
+print("file:", filename)
+
 img_file = Image.open(filename)
 img = img_file.load()
 
@@ -46,11 +46,11 @@ if cl == "kmeans":
     print(kmeans.cluster_centers_)
     Y = kmeans.predict(X)
     print(Y)
-    Y = np.transpose(np.reshape(Y, (width,height)))
+    Y = np.transpose(np.reshape(Y, (width, height)))
     plt.figure()
-    plt.subplot(2,2,1)
+    plt.subplot(2, 2, 1)
     plt.imshow(mpimg.imread(filename))
-    plt.subplot(2,2,2)
+    plt.subplot(2, 2, 2)
     plt.imshow(Y)
 
 if cl == "DBSCAN":
@@ -78,26 +78,25 @@ if cl == "DBSCAN":
         class_member_mask = (labels == k)
 
         xy = X[class_member_mask & core_samples_mask]
-        #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),markeredgecolor='k', markersize=14)
+        # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),markeredgecolor='k', markersize=14)
 
         xy = X[class_member_mask & ~core_samples_mask]
-        #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),markeredgecolor='k', markersize=6)
+        # plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),markeredgecolor='k', markersize=6)
 
     plt.title('Estimated number of clusters: %d' % n_clusters_)
-    #plt.show()
+    # plt.show()
     plt.imshow(np.transpose(np.reshape(labels, (width, height))))
     plt.show()
-
 
 # do the actual cropping:
 
 # find the boundaries of the whale tail:
 #
-#left = 999999
-#right = 0
-#top = 999999
-#bottom = 0
-#for row in Y:
+# left = 999999
+# right = 0
+# top = 999999
+# bottom = 0
+# for row in Y:
 #    row = list(row)
 #    if row.index(1) < left:
 #        left = row.index(1)
@@ -105,12 +104,12 @@ if cl == "DBSCAN":
 #    if row.index(1) < right:
 #        right = row.index(1)
 #
-#print(left, right)
+# print(left, right)
 
 # nearest neighbour via majority vote
 window = 1
 print(Y)
-copy = np.zeros(shape=(len(Y),len(Y[0])))
+copy = np.zeros(shape=(len(Y), len(Y[0])))
 for index_row, row in enumerate(Y):
     for index_col, col in enumerate(row):
         votes = [0] * nr_clusters
@@ -120,18 +119,18 @@ for index_row, row in enumerate(Y):
                     continue
                 else:
                     votes[Y[index_row + x][index_col + y]] += 1
-                
-#        print(index_row)        
-#        Y[index_row][index_col] = votes.index(max(votes))
+
+        #        print(index_row)
+        #        Y[index_row][index_col] = votes.index(max(votes))
         copy[index_row][index_col] = votes.index(max(votes))
-        
-plt.subplot(2,2,3)
+
+plt.subplot(2, 2, 3)
 plt.imshow(copy)
 
 # do it a second time to smooth it out even more:
-copy2 = np.zeros(shape=(len(copy),len(copy[0])))
+copy2 = np.zeros(shape=(len(copy), len(copy[0])))
 for index_row, row in enumerate(copy):
-    for index_col, col in enumerate(copy):
+    for index_col, col in enumerate(row):
         votes = [0] * nr_clusters
         for x in range(-window, window):
             for y in range(-window, window):
@@ -139,12 +138,62 @@ for index_row, row in enumerate(copy):
                     continue
                 else:
                     votes[int(copy[index_row + x][index_col + y])] += 1
-                
-#        print(index_row)        
-#        Y[index_row][index_col] = votes.index(max(votes))
+
+        #        print(index_row)
+        #        Y[index_row][index_col] = votes.index(max(votes))
         copy2[index_row][index_col] = votes.index(max(votes))
-        
-plt.subplot(2,2,4)
-plt.imshow(copy)
-plt.show()
+
+plt.subplot(2, 2, 4)
+plt.imshow(copy2)
+#plt.show()
+# note: smoothing seems to work, but it does not really make the distinction between tail and not tail better. It
+# remains questionable if we should really use that.
+
+# Find the biggest area of one color:
+Y = copy2
+resp = np.zeros(shape=(len(Y), len(Y[0])))
+
+def mark_group(row, col, val, group_nr):
+    if row < 0 or row >= len(Y) or col < 0 or col >= len(Y[0]):
+        return
+    if resp[row, col] != 0:
+        return
+    if Y[row,col] == val:
+        # mark the actual position
+        resp[row,col] = group_nr
+        # mark all the neigbours
+        mark_group(row + 1, col, val, group_nr)
+        mark_group(row, col + 1, val, group_nr)
+        mark_group(row - 1, col, val, group_nr)
+        mark_group(row, col - 1, val, group_nr)
+
+area = 1
+for index_row, row in enumerate(Y):
+    for index_col, col in enumerate(row):
+        if resp[index_row][index_col] == 0:
+            # if that cell is not assigned a group yet, assign it to one:
+            group_val = Y[index_row][index_col]
+            mark_group(index_row, index_col, group_val, area)
+            area += 1
+
+out = open("bigfile.txt", 'w')
+for line in resp:
+    for val in line:
+        out.write(str(int(val)) + " ")
+    out.write("\n")
+    #print(line)
+
+# now we have a representation of all distinctive fields of the clustering results. Look in "bigfile.txt to see it;
+# show now how big the 2 biggest areas are; 2 because the water is most likely to be the biggest one
+area_dict = {}
+for row in resp:
+    for val in row:
+        if str(val) in area_dict:
+            area_dict[str(val)] += 1
+        else:
+            area_dict[str(val)] = 1
+
+print(list(sorted(area_dict.values())))
+
+# From here on we have to identify which area is the whale and crop that out.
 
